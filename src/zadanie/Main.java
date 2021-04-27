f55e3694010fc690d9ce4ec63ecb19dc6c72f8e8
Arkadiusz Kaim <arekkaim1997@gmail.com>
2021-04-27 15:08:59 +0200

    a

--- src/zadanie/Main.java
+++ src/zadanie/Main.java
@@ -1,8 +1,11 @@
-package lekcja8;
+package zadanie;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("XD");
+        System.out.println("XD");
+        System.out.println("XD");
+
         System.out.println("XD");
         System.out.println("XD");
     }
